5f839dda0c6bc171de579ea05b33118b7e466868
JubaerHosain <jubaerhosain1119@gmail.com>
2023-07-03 10:26:04 +0600

    repo update

---
+++ front-end-react-bootstrap/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Modal from "./components/modal/Modal";
+
+function App() {
+  return (
+    <div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default App;
