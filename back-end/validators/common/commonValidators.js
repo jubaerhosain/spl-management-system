270bd18c012efd938876aa26e12895703cfbe86c
JubaerHosain <jubaerhosain1119@gmail.com>
2023-11-21 14:33:50 +0600

    added joi

--- back-end/validators/common/commonValidators.js
+++ back-end/validators/common/commonValidators.js
@@ -1,19 +1,16 @@
-import UserRepository from "../../repositories/UserRepository.js";
-import CustomError from "../../utils/CustomError.js";
-
 export const validatePassword = (password, helper) => {
-    if (password.length < 8) return helper.message("Password length must be at least 8 character");
-    if (password.length > 30) return helper.message("Password length must be less than 30 character");
+    if (password.length < 8) return helper.message("password length must be at least 8 character");
+    if (password.length > 30) return helper.message("password length must be less than 30 character");
 
     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&.]{8,}$/;
     if (!regex.test(password))
-        return helper.message("Must contain at least one uppercase, lowercase, number and special character");
+        return helper.message("password must contain at least one uppercase, lowercase, number and special character");
 
     return true;
 };
 
 export const validateName = (name, helper) => {
-    if (name.length < 3) return helper.message("Name must be at least 3 characters");
+    if (name.length < 3) return helper.message("name must be at least 3 character long");
 
     let regex = /^[ \.a-zA-Z]{3,}$/;
     if (!regex.test(name)) {
@@ -26,41 +23,27 @@ export const validateEmail = (email, helper) => {
     const regex = /.+@iit\.du\.ac\.bd$/;
 
     if (!regex.test(email)) {
-        return helper.message("Must be end with '@iit.du.ac.bd");
+        return helper.message("email must be end with '@iit.du.ac.bd");
     }
 
     return true;
 };
 
-export const validatePhoneNumber = (phoneNumber, helper) => {
+export const validatePhoneNumber = (phone, helper) => {
     const regex = /(^\+8801[0-9]{9}$)|(^01[0-9]{9}$)/;
 
-    if (!regex.test(phoneNumber)) {
-        return helper.message("Must be a bangladeshi number");
+    if (!regex.test(phone)) {
+        return helper.message("phone must be a bangladeshi number");
     }
 
     return true;
-};
-
-export const checkEmailExistence = async (email) => {
-    try {
-        const exists = await UserRepository.isEmailExists(email);
-        if (!exists) {
-            throw new CustomError("Email does not exist", 400);
-        }
-        return true;
-    } catch (err) {
-        console.log(err);
-        if (err.status) throw new CustomError(err.message);
-        else throw new CustomError("And error occurred while checking email");
-    }
 };
 
 export const validateGender = (gender, helper) => {
     const options = ["male", "female", "other"];
 
     if (!options.includes(gender)) {
-        return helper.message("Must be in ['male', 'female', 'other']");
+        return helper.message("gender must be in ['male', 'female', 'other']");
     }
 
     return true;
@@ -71,7 +54,7 @@ export const validateRollNo = (rollNo, helper) => {
     const isValid = regex.test(rollNo);
 
     if (!regex.test(rollNo)) {
-        return helper.message("Must be a 4 digit number");
+        return helper.message("rollNo must be a 4 digit number");
     }
 
     return true;
@@ -81,7 +64,7 @@ export const validateRegistrationNo = (registrationNo, helper) => {
     const regex = /^[0-9]{10}$/;
 
     if (!regex.test(registrationNo)) {
-        return helper.message("Must be a 10 digit number");
+        return helper.message("registrationNo must be a 10 digit number");
     }
 
     return true;
@@ -91,7 +74,7 @@ export const validateBatch = (batch, helper) => {
     const regex = /^[0-9]{2}$/;
 
     if (!regex.test(batch)) {
-        return helper.message("Must be a 2 digit number");
+        return helper.message("batch must be a 2 digit number");
     }
 
     return true;
@@ -101,7 +84,7 @@ export const validateSession = (session, helper) => {
     const regex = /^[0-9]{4}-[0-9]{2}$/;
 
     if (!regex.test(session)) {
-        return helper.message("Must be in the following format: '2018-19'");
+        return helper.message("session must be in the following format: '2018-19'");
     }
 
     return true;
@@ -111,7 +94,7 @@ export const validateCurriculumYear = (curriculumYear, helper) => {
     const options = ["1st", "2nd", "3rd", "4th"];
 
     if (!options.includes(curriculumYear)) {
-        return helper.message("Must be in ['1st', '2nd', '3rd', '4th']");
+        return helper.message("curriculumYear must be in ['1st', '2nd', '3rd', '4th']");
     }
 
     return true;
@@ -121,7 +104,7 @@ export const validateSPLName = (splName, helper) => {
     const options = ["spl1", "spl2", "spl3"];
 
     if (!options.includes(splName)) {
-        return helper.message("Must be in ['spl1', 'spl2', 'spl3'");
+        return helper.message("splName must be in ['spl1', 'spl2', 'spl3'");
     }
 
     return true;
@@ -131,7 +114,16 @@ export const validateAcademicYear = (academicYear, helper) => {
     const regex = /^[0-9]{4}-[0-9]{2}$/;
 
     if (!regex.test(academicYear)) {
-        return helper.message("Must be in following format: '2018-19'");
+        return helper.message("academicYear must be in following format: '2018-19'");
+    }
+
+    return true;
+};
+
+export const validateOTP = (otp, helper) => {
+    const regex = /^\d{6}$/;
+    if (!regex.test(otp)) {
+        return helper.message("otp must be a 6-digit number");
     }
 
     return true;
--- back-end/validators/studentValidator.js
+++ back-end/validators/studentValidator.js
@@ -1,6 +1,4 @@
 import joi from "joi";
-import { body } from "express-validator";
-import StudentRepository from "../repositories/StudentRepository.js";
 
 const Joi = joi.defaults((schema) => {
     return schema.options({
@@ -20,16 +18,6 @@ import {
     validateCurriculumYear,
 } from "./common/commonValidators.js";
 
-import {
-    checkAddStudentUniqueness,
-    checkAddStudentExistence,
-    requiredAtLeastOneField,
-    isFieldAllowed,
-} from "./common/validationMiddlewares.js";
-
-const allowedFieldsForStudent = ["phone", "name", "gender", "details"];
-const allowedFieldsForAdmin = ["rollNo", "registrationNo", "batch", "session", "curriculumYear"];
-
 const createStudentSchema = Joi.array().items(
     Joi.object({
         name: Joi.string().trim().custom(validateName).required(),
@@ -42,177 +30,24 @@ const createStudentSchema = Joi.array().items(
     })
 );
 
-const validateCreateStudentAccount = [
-    body("students").isArray().withMessage("Must be an array").bail().isLength({ min: 1 }).withMessage("Cannot be empty array"),
+const updateStudentSchema = Joi.object({
+    name: Joi.string().trim().custom(validateName).optional(),
+    gender: Joi.string().trim().custom(validateGender).optional(),
+    phone: Joi.string().trim().custom(validatePhoneNumber).optional(),
+    details: Joi.string().trim().min(5).max(600).optional(),
+});
 
-    body("students.*.name")
-        .trim()
-        .notEmpty()
-        .withMessage("Name cannot be empty")
-        .custom((name) => {
-            return validateName(name);
-        }),
-
-    body("students.*.email")
-        .trim()
-        .notEmpty()
-        .withMessage("Email cannot be empty")
-        .isEmail()
-        .withMessage("Invalid email format")
-        .bail()
-        .custom((email) => {
-            return validateEmail(email);
-        }),
-
-    body("students.*.rollNo")
-        .trim()
-        .notEmpty()
-        .withMessage("rollNo cannot be empty")
-        .custom((rollNo) => {
-            return validateRollNo(rollNo);
-        }),
-
-    body("students.*.registrationNo")
-        .trim()
-        .notEmpty()
-        .withMessage("registrationNo cannot be empty")
-        .custom((registrationNo) => {
-            return validateRegistrationNo(registrationNo);
-        }),
-
-    body("students.*.batch")
-        .trim()
-        .notEmpty()
-        .withMessage("batch cannot be empty")
-        .custom((batch) => {
-            return validateBatch(batch);
-        }),
-
-    body("students.*.session")
-        .trim()
-        .notEmpty()
-        .withMessage("session cannot be empty")
-        .custom((session) => {
-            return validateSession(session);
-        }),
-
-    body("students.*.curriculumYear")
-        .trim()
-        .notEmpty()
-        .withMessage("curriculumYear cannot be empty")
-        .custom((curriculumYear) => {
-            return validateCurriculumYear(curriculumYear);
-        }),
-
-    checkAddStudentUniqueness,
-
-    checkAddStudentExistence,
-];
-
-const validateUpdateStudentAccount = [
-    requiredAtLeastOneField,
-    isFieldAllowed(allowedFieldsForStudent),
-
-    body("name")
-        .trim()
-        .notEmpty()
-        .withMessage("Name must be provided")
-        .bail()
-        .custom((name) => {
-            return validateName(name);
-        })
-        .optional(),
-
-    body("gender")
-        .trim()
-        .notEmpty()
-        .withMessage("Name must be provided")
-        .custom((gender) => {
-            return validateGender(gender);
-        })
-        .optional(),
-
-    body("phone")
-        .trim()
-        .notEmpty()
-        .withMessage("Name must be provided")
-        .custom((phone) => {
-            return validatePhoneNumber(phone);
-        })
-        .optional(),
-
-    body("details").trim().notEmpty().withMessage("Details must be provided").optional(),
-];
-
-const validateUpdateStudentAccountByAdmin = [
-    requiredAtLeastOneField,
-    isFieldAllowed(allowedFieldsForAdmin),
-
-    body("rollNo")
-        .trim()
-        .custom((rollNo) => {
-            return validateRollNo(rollNo);
-        })
-        .bail()
-        .custom(async (rollNo) => {
-            try {
-                const exist = await StudentRepository.isRollNoExist(rollNo);
-                if (exist) throw new Error("rollNo already exists", 400);
-            } catch (err) {
-                if (err.status) throw new Error(err.message);
-                else {
-                    console.log(err);
-                    throw new Error("An error occurred while checking rollNo");
-                }
-            }
-        })
-        .optional(),
-
-    body("registrationNo")
-        .trim()
-        .custom((registrationNo) => {
-            return validateRegistrationNo(registrationNo);
-        })
-        .bail()
-        .custom(async (registrationNo) => {
-            try {
-                const exist = await StudentRepository.isRegistrationNoExist(registrationNo);
-                if (exist) throw new Error("registrationNo already exists", 400);
-            } catch (err) {
-                if (err.status) throw new Error(err.message);
-                else {
-                    console.log(err);
-                    throw new Error("An error occurred while checking registrationNo");
-                }
-            }
-        })
-        .optional(),
-
-    body("batch")
-        .trim()
-        .custom((batch) => {
-            return validateBatch(batch);
-        })
-        .optional(),
-
-    body("session")
-        .trim()
-        .custom((session) => {
-            return validateSession(session);
-        })
-        .optional(),
-
-    body("curriculumYear")
-        .trim()
-        .custom((curriculumYear) => {
-            return validateCurriculumYear(curriculumYear);
-        })
-        .optional(),
-];
+const updateStudentByAdminSchema = Joi.object({
+    name: Joi.string().trim().custom(validateName).optional(),
+    rollNo: Joi.string().trim().custom(validateRollNo).optional(),
+    registrationNo: Joi.string().trim().custom(validateRegistrationNo).optional(),
+    batch: Joi.string().trim().custom(validateBatch).optional(),
+    session: Joi.string().trim().custom(validateSession).optional(),
+    curriculumYear: Joi.string().trim().custom(validateCurriculumYear).optional(),
+});
 
 export default {
     createStudentSchema,
-    validateCreateStudentAccount,
-    validateUpdateStudentAccount,
-    validateUpdateStudentAccountByAdmin,
+    updateStudentSchema,
+    updateStudentByAdminSchema,
 };
