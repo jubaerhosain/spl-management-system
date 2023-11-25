e8f9cec2d97d850a10e9c8de62fb7890cc72709e
JubaerHosain <jubaerhosain1119@gmail.com>
2023-11-25 18:18:28 +0600

    updated user routes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -4,8 +4,19 @@ const userRoutes = express.Router();
 import userController from "../controllers/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
+// user related routes
 userRoutes.post("/", authMiddleware.checkAuthentication, authMiddleware.isAdmin, userController.createUser);
-userRoutes.put("/", authMiddleware.checkAuthentication, authMiddleware.isAdmin, userController.updateUser);
+userRoutes.get("/");
+userRoutes.get("/:userId");
+userRoutes.put("/:userId", authMiddleware.checkAuthentication, authMiddleware.isAdmin, userController.updateUser);
+userRoutes.put("/:userId/avatar");
+userRoutes.put("/:userId/activate");
+userRoutes.put("/:userId/deactivate");
 userRoutes.delete("/:userId", authMiddleware.checkAuthentication, authMiddleware.isAdmin, userController.deleteUser);
+
+// notification related routes
+userRoutes.get("/:userId/notification");
+userRoutes.put("/:userId/notification");
+userRoutes.delete("/:userId/notification/:notificationId");
 
 export default userRoutes;
