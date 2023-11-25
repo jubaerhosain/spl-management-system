ed691d6b4e970174eae96c02d8dfbfe57bdeb26d
JubaerHosain <jubaerhosain1119@gmail.com>
2023-11-25 19:02:13 +0600

    updated swagger path files

--- back-end/config/swagger.js
+++ back-end/config/swagger.js
@@ -4,21 +4,39 @@ import YAML from "yamljs";
 import path from "path";
 import { getDirectoryName } from "../utils/fileUtils.js";
 const mainSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/main.yaml");
-const usersSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/users.yaml");
-const postsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/posts.yaml");
 const componentsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/components.yaml");
+const authSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/auth.yaml");
+const usersSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/users.yaml");
+const studentsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/students.yaml");
+const teachersSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/teachers.yaml");
+const splsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/spls.yaml");
+const teamsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/teams.yaml");
+const marksSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/marks.yaml");
+const noticesSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/notices.yaml");
 
 const mainSpec = YAML.load(mainSpecUrl);
+const authSpec = YAML.load(authSpecUrl);
 const usersSpec = YAML.load(usersSpecUrl);
-const postsSpec = YAML.load(postsSpecUrl);
+const studentsSpec = YAML.load(studentsSpecUrl);
+const teachersSpec = YAML.load(teachersSpecUrl);
+const splsSpec = YAML.load(splsSpecUrl);
+const marksSpec = YAML.load(mainSpecUrl);
+const teamsSpec = YAML.load(teamsSpecUrl);
+const noticesSpec = YAML.load(noticesSpecUrl);
 const componentsSpec = YAML.load(componentsSpecUrl);
 
 const apiSpec = {
     ...mainSpec,
     paths: {
         ...mainSpec.paths,
+        ...authSpec.paths,
         ...usersSpec.paths,
-        ...postsSpec.paths,
+        ...teachersSpec.paths,
+        ...studentsSpec.paths,
+        ...teamsSpec.paths,
+        ...splsSpec.paths,
+        ...noticesSpec.paths,
+        ...marksSpec.paths,
     },
     components: {
         ...mainSpec.components,
