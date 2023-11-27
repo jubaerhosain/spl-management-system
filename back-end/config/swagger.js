c317ae14a6d61fe0cda118515c82e05a93b9f5dd
JubaerHosain <jubaerhosain1119@gmail.com>
2023-11-27 14:11:12 +0600

    updated

--- back-end/config/swagger.js
+++ back-end/config/swagger.js
@@ -4,7 +4,6 @@ import YAML from "yamljs";
 import path from "path";
 import { getDirectoryName } from "../utils/fileUtils.js";
 const mainSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/main.yaml");
-const componentsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/components.yaml");
 const authSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/auth.yaml");
 const usersSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/users.yaml");
 const studentsSpecUrl = path.join(getDirectoryName(import.meta.url), "../api/docs/paths/students.yaml");
@@ -23,7 +22,6 @@ const splsSpec = YAML.load(splsSpecUrl);
 const marksSpec = YAML.load(mainSpecUrl);
 const teamsSpec = YAML.load(teamsSpecUrl);
 const noticesSpec = YAML.load(noticesSpecUrl);
-const componentsSpec = YAML.load(componentsSpecUrl);
 
 const apiSpec = {
     ...mainSpec,
@@ -40,7 +38,6 @@ const apiSpec = {
     },
     components: {
         ...mainSpec.components,
-        ...componentsSpec.components,
     },
 };
 
